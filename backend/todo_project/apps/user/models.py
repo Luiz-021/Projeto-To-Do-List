055f201514500ad04c9e36ee7dff5dce0533f047
Luiz-021 <luizgute@outlook.com>
2025-05-28 13:32:02 -0300

    Ajustes finais

--- backend/todo_project/apps/user/models.py
+++ backend/todo_project/apps/user/models.py
@@ -2,12 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    """
-    Herdamos AbstractUser para já ter:
-      username, password(hash), email,
-      first_name, last_name, is_active, is_staff,
-      is_superuser, last_login, date_joined.
-    """
     def authenticate(self, raw_password):
         return self.check_password(raw_password)
 
