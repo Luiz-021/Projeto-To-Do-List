a5c4194428e81d3669f4143ac371be86bbdf31a7
lucas <lucasvbento1@gmail.com>
2025-05-27 21:54:24 -0300

    Configurando View de criaçao de usuario

--- backend/todo_project/apps/ToDoList/views.py
+++ backend/todo_project/apps/ToDoList/views.py
@@ -7,11 +7,15 @@ from .serializers import TaskSerializer
 class TaskListCreateView(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-
+    permission_classes = [IsAuthenticated]
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 class TaskRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+    
 
 
 class TaskUserView(generics.ListAPIView):
